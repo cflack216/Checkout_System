309069a4abadf05c2268912e4077f02b571f3990
cflack216 <2775446@vikes.csuohio.edu>
2022-10-19 14:16:55 -0400

    Initial Commit

--- item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-
-    def __init__(self, price, name, upc):
-        self.price = price
-        self.description = name
-        self.UPC = upc
-
-    def scan(self):
-        print(self.description + " $" + str(self.price))
--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import inventory
+class Inventory:
+    shelf = []
+    def __init__(self):
+        self.shelf.append(Item(3.99, "bread", 1231))
+        self.shelf.append(Item(2.99, "chocolate", 9999))
+        self.shelf.append(Item(6.99, "charger", 7874))
+        self.shelf.append(Item(9.99, "book", 9785))
+        self.shelf.append(Item(19.99, "model", 2934))
 
-x = inventory.Inventory()
-total = 0
+    def search(self, UPC):
+        for i in self.shelf:
+            if i.UPC == UPC:
+                return i
+        return Item(999, "sentinel", 1234)
+
+
+class Item:
+    def __init__(self, price, name, upc):
+        self.price = price
+        self.description = name
+        self.UPC = upc
+
+    def scan(self):
+        print(self.description + " $" + str(self.price))
+
+
+x = Inventory()
+total = 0.00
 
 while True:
     try:
@@ -10,7 +34,8 @@ while True:
         break
     item = x.search(UPC)
     item.scan()
-    total+=item.price
+    total += item.price
 
-total+=(.08*total)
+total += (.08 * total)
+total = round(total,2)
 print("total $" + str(total))
